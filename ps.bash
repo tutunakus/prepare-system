e298d0dfebcb7c5a5e34223f0d2228665da0cf14
naktu <dk@tutunak.com>
2015-05-19 05:08:44 +0300

    add keepass in software list

--- ps.bash
+++ ps.bash
@@ -4,7 +4,7 @@ OS_RELEASE=/etc/os-release				# include file for define distributive OS
 
 # list variables with software
 UNI_SOFT="vim htop mc openvpn wget"			# universal soft list for all type installation
-DESK_LIST="moc libreoffice dropbox yandex-disk anki cherrytree"		# software for desktop installation
+DESK_LIST="moc libreoffice dropbox yandex-disk anki cherrytree keepass"		# software for desktop installation
 
 # list variables with of distribution names
 FEDORA=Fedora
