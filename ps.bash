5cfcc667d30ea28f33b4c64b56f294545cbf2c5a
naktu <dk@tutunak.com>
2015-07-12 13:35:26 +0300

    ps.bash

--- ps.bash
+++ ps.bash
@@ -27,7 +27,7 @@ function gent {						# if you use gentoo
 function cent {						# if you use centos
 	echo "somethink for centos"
 	SOFT_LIST=$UNI_SOFT
-	yum update
+	yum update -y
 	yum install epel-release -y
 	yum install -y $SOFT_LIST
 }
